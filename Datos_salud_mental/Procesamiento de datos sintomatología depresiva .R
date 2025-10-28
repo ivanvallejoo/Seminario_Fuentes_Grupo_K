f1c312cd5c803484675121d44cd1eeacaa68ae86
Diego-UL <diegougartelopez@gmail.com>
2025-10-28 12:05:34 +0100

    Funcion leer CSV actualizada con distintas con diferentes codificaciones

--- Datos_salud_mental/Procesamiento de datos sintomatología depresiva .R
+++ Datos_salud_mental/Procesamiento de datos sintomatología depresiva .R
@@ -6,13 +6,23 @@ leer_csvs_objetos <- function(directorio, patron = "\\.csv$") {
   archivos <- list.files(path = directorio, pattern = patron, full.names = TRUE)
   
   for (archivo in archivos) {
-    nombre <- tools::file_path_sans_ext(basename(archivo))  # nombre del archivo sin extensión
+    nombre <- tools::file_path_sans_ext(basename(archivo))
     
-    datos <- read_delim(archivo, delim = ";", locale = locale(encoding = "Latin1"))
+    # Intentar primero con UTF-8, si falla probar con Latin1
+    datos <- tryCatch(
+      read_delim(archivo, delim = ";", locale = locale(encoding = "UTF-8")),
+      error = function(e) {
+        read_delim(archivo, delim = ";", locale = locale(encoding = "Latin1"))
+      }
+    )
     
-    assign(nombre, datos, envir = .GlobalEnv)  # crea el objeto en el entorno global
+    # Limpiar nombres de columnas (quita espacios, acentos, etc.)
+    colnames(datos) <- make.names(colnames(datos))
+    
+    assign(nombre, datos, envir = .GlobalEnv)
   }
 }
+
 
 # Ejemplo de uso:
 leer_csvs_objetos("C:/Users/diego/Downloads/Seminario_Fuentes_Grupo_K/Datos_salud_mental")
