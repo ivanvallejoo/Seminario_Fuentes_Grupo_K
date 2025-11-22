fabdffde17a09ad81b63602ce6b54ddd1396c5af
ivanvallejoo <ivanvallejocabrero@gmail.com>
2025-11-22 10:21:39 +0100

    modificación mapa España

--- TICS_gráficos/Mapa_España_TICS.R
+++ TICS_gráficos/Mapa_España_TICS.R
@@ -1,113 +1,81 @@
-library(sf)
-library(dplyr)
 library(ggplot2)
 library(plotly)
+library(dplyr)
 library(mapSpain)
+library(sf)
+
 
 mapa_españa_ccaa <- esp_get_ccaa(moveCAN = TRUE)
 
-View(mapa_españa_ccaa)
 
-uso_tic_total_corregido <- uso_tic_total %>%
+# Corrección para mapa
+uso_tic_total_corregido_mapa <- uso_tic_total %>%
   mutate(
     Comunidad = case_when(
-      Comunidad == "Andalucía" ~ "Andalucía",
-      Comunidad == "Aragón" ~ "Aragón",
-      Comunidad == "Asturias, Principado de" ~ "Asturias",
+      Comunidad == "Asturias, Principado de"~"Asturias",
       Comunidad == "Balears, Illes" ~ "Baleares",
-      Comunidad == "Canarias" ~ "Canarias",
-      Comunidad == "Cantabria" ~ "Cantabria",
-      Comunidad == "Castilla y León" ~ "Castilla y León",
-      Comunidad == "Castilla - La Mancha" ~ "Castilla - La Mancha",
+      Comunidad == "Castilla - La Mancha" ~ "Castilla-La Mancha",
       Comunidad == "Comunitat Valenciana" ~ "Comunidad Valenciana",
-      Comunidad == "Extremadura" ~ "Extremadura",
-      Comunidad == "Galicia" ~ "Galicia",
       Comunidad == "Madrid, Comunidad de" ~ "Madrid",
       Comunidad == "Murcia, Región de" ~ "Murcia",
       Comunidad == "Navarra, Comunidad Foral de" ~ "Navarra",
-      Comunidad == "País Vasco" ~ "País Vasco",
       Comunidad == "Rioja, La" ~ "La Rioja",
-      Comunidad == "Ceuta" ~ "Ceuta",
-      Comunidad == "Melilla" ~ "Melilla",
-      
       TRUE ~ Comunidad
     )
   )
 
-View(uso_tic_total)
-
 mapa_con_datos <- mapa_españa_ccaa %>%
-  left_join(uso_tic_total_corregido, by = c("ccaa.shortname.es" = "Comunidad"))
-View(mapa_con_datos)
+  left_join(uso_tic_total_corregido_mapa, by = c("ccaa.shortname.es" = "Comunidad"))
 
-mapa_con_datos <- mapa_con_datos %>%
+
+
+
+datos_mapa_coloreado <- mapa_con_datos %>%
   mutate(
+    Uso_Global_Indice = (Frecuencia_Total_Adultos+ Frecuencia_Total_Niños+ Frecuencia_Total_Mayores)/3,
+    
+    
     tooltip_data = paste0(
-      'Comunidad Autónoma: ', ccaa.shortname.es, "<br>", 
-      "Uso Adultos: ", round(Frecuencia_Total_Adultos, 2), "%<br>",
-      "Uso Niños: ", round(Frecuencia_Total_Niños, 2), "%<br>",
-      "Uso Mayores: ", round(Frecuencia_Total_Mayores, 2), "%"
+      '<b>', ccaa.shortname.es, "</b><br>", 
+      "--------------------------<br>",
+      "Índice Global: ", round(Uso_Global_Indice, 1), "%<br>",
+      "Adultos: ", round(Frecuencia_Total_Adultos, 1), "%<br>",
+      "Niños: ", round(Frecuencia_Total_Niños, 1), "%<br>",
+      "Mayores: ", round(Frecuencia_Total_Mayores, 1), "%"
     )
   )
 
-colores_ccaa <- c(
-  "Andalucía" = "#C0C0C0",           
-  "Aragón" = "#FFD700",              
-  "Asturias" = "#0000FF",            
-  "Baleares" = "#800080",            
-  "Canarias" = "#FFA500",            
-  "Cantabria" = "#008000",           
-  "Castilla y León" = "#B0C4DE",     
-  "Castilla-La Mancha" = "#FF0000",
-  "Comunidad Valenciana" = "#ADD8E6",
-  "Extremadura" = "#8B4513",         
-  "Galicia" = "#40E0D0",            
-  "Madrid" = "#FFFF00",              
-  "Murcia" = "#FFC0CB",              
-  "Navarra" = "#A52A2A",             
-  "País Vasco" = "#00FF00",          
-  "La Rioja" = "#DA70D6",            
-  "Ceuta" = "#4682B4",               
-  "Melilla" = "#5F9EA0",
-  "Cataluña" = "#FFDAB9"
-  
-)
-
-
-mapa_estatico_gg <- ggplot(data = mapa_con_datos) +
-  
+mapa_intensidad <- ggplot(data = datos_mapa_coloreado) +
   geom_sf(
-    aes(fill = ccaa.shortname.es, text = tooltip_data, key = ccaa.shortname.es),
-    color = NA, 
-    size = 0 
+    aes(fill = Uso_Global_Indice, text = tooltip_data),
+    color = "white", size = 0.2 
   ) +
-  
   geom_sf_text(
     aes(label = ccaa.shortname.es), 
-    color = "black", 
-    size = 2 
+    size = 2,         
+    color = "black",     
+    fontface = "bold",
+    check_overlap = TRUE 
+  ) +
+
+  scale_fill_gradient(
+    low = "#EBF5FB",   
+    high = "#21618C",  
+    name = "Intensidad de Uso"
   ) +
   
-  scale_fill_manual(values = colores_ccaa) +
   theme_void() +
-  theme(legend.position = "none")
-
-
-mapa_interactivo_gg <- ggplotly(
-  mapa_estatico_gg,
-  tooltip = "text" 
-)
-
-mapa_interactivo_final <- mapa_interactivo_gg %>%
-  style(hoveron = "fills", traces = 1) %>% 
-
-  layout(
-    hoverlabel = list(
-      bgcolor = "white",
-      bordercolor = "black",
-      font = list(color = "black")
-    )
+  labs(title = "Intensidad del uso de TICs en España ") +
+  theme(
+    legend.position = "right",
+    plot.title = element_text(hjust = 0.5, face = "bold")
   )
 
 
-mapa_interactivo_final
+mapa_final_españa <- ggplotly(mapa_intensidad, tooltip = "text") %>%
+  style(hoveron = "fills", traces = 1) %>% 
+  layout(
+    hoverlabel = list(bgcolor = "white", bordercolor = "black", font = list(color = "black"))
+  )
+
+mapa_final_españa
